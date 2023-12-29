99b0e1651755e0cd520ca603cb988e4f43814d1a
BeeTillman <bpt3creations@gmail.com>
2023-12-29 01:04:01 -0500

    Add new positioned player classes
    Add QuarterBack, RunningBack, TightEnd, and WideReceiver classes to the PositionedPlayers package.

---
+++ src/Components/Players/PositionedPlayers/RunningBack.java
@@ -0,0 +1,10 @@
+package Components.Players.PositionedPlayers;
+
+import Components.Players.Player;
+import Components.Players.Position;
+
+public class QuarterBack extends Player {
+    public QuarterBack(Position position) {
+        super(position);
+    }
+}
---
+++ src/Components/Players/PositionedPlayers/WideReceiver.java
@@ -0,0 +1,10 @@
+package Components.Players.PositionedPlayers;
+
+import Components.Players.Player;
+import Components.Players.Position;
+
+public class RunningBack extends Player {
+    public RunningBack(Position position) {
+        super(position);
+    }
+}
